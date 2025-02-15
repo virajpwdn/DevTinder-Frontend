abb2823735f7ba4226c71d7755c785f5f6368d05
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-16 03:44:52 +0530

    created routes setup, we have done routes setup in app.js and created a structure for routes inside of body.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 const App = () => {
   return (
-    <div className="navbar bg-base-300">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">👨🏻‍💻 DevTinder</a>
-      </div>
-      <div className="flex-none gap-2">
-        
-        <div className="dropdown dropdown-end mx-5">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 

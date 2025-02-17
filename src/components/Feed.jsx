3f13cb28b91f6169ba940ce26969ceb60ceee5b7
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-17 23:37:56 +0530

    showing feed with token

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { BASE_URL } from '../utils/constants';
+
 
 const Feed = () => {
+
   return (
     <div>Feed</div>
   )

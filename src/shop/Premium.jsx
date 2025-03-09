643945aa49cc3982dfd9eb9ef9ae3a46f54113cb
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-09 13:24:33 +0530

    connected with backend new branch

--- src/shop/Premium.jsx
+++ src/shop/Premium.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { BASE_URL } from '../utils/constants'
 
 const Premium = () => {
+    const [data, setData] = useState("")
+    
+    axios.get(BASE_URL + "/shop/premium", {withCredentials:true}).then((res)=>{
+        console.log(res.data);
+        setData(res.data)        
+    }).catch(error => console.log(error))
   return (
-    <div>Premium</div>
+    <div>
+        {data && <h1>{data}</h1>}
+    </div>
   )
 }
 

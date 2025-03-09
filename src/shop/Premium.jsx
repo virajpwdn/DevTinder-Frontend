0aca16643a4d31850e144edb4508b3342c9a7fb9
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-09 13:50:21 +0530

    completed UI for premium users

--- src/shop/Premium.jsx
+++ src/shop/Premium.jsx
@@ -1,19 +1,46 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { BASE_URL } from '../utils/constants'
+import axios from "axios";
+import React, { useState } from "react";
+import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
-    const [data, setData] = useState("")
-    
-    axios.get(BASE_URL + "/shop/premium", {withCredentials:true}).then((res)=>{
-        console.log(res.data);
-        setData(res.data)        
-    }).catch(error => console.log(error))
   return (
-    <div>
-        {data && <h1>{data}</h1>}
+    <div className="h-screen">
+      <div className="flex w-full flex-col lg:flex-row mt-10 p-5">
+        <div className="card bg-base-300 rounded-box py-10 grid grow place-items-center">
+          <h1 className="font-bold text-3xl uppercase mb-10">
+            Silver <span className="text-sky-400 underline">Membership</span>
+          </h1>
+          <ol className="list-decimal">
+            <li>50 Backtracking per week</li>
+            <li>6 Months Access</li>
+            <li>Send Direct Message to 20 people per day</li>
+            <li>Share Project Ideas on feed</li>
+            <li>Connect Git Account to share projects</li>
+          </ol>
+          <button className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10">
+            Pay Now
+          </button>
+        </div>
+        <div className="divider lg:divider-horizontal">OR</div>
+        <div className="card bg-base-300 rounded-box grid py-10 grow place-items-center">
+          <h1 className="font-bold text-3xl mb-10 uppercase">
+            Gold <span className="text-yellow-500 underline">Membership</span>
+          </h1>
+          <ol className="list-decimal">
+            <li>Get a verified Blue Tick</li>
+            <li>Unlimited Backtracking per week</li>
+            <li>12 Months Access</li>
+            <li>Send Unlimited Direct Message</li>
+            <li>Share Project Ideas on feed</li>
+            <li>Connect Git Account to share projects</li>
+          </ol>
+          <button className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10">
+            Pay Now
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Premium
+export default Premium;

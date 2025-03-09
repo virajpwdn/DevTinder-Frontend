b427fb3ea7ca8836d1563269fdebee5bf57b64de
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-09 19:49:36 +0530

    done user premium verification

--- src/shop/Premium.jsx
+++ src/shop/Premium.jsx
@@ -1,9 +1,28 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
   const [error, setError] = useState("");
+  const [premiumUser, setPremiumUser] = useState(false);
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  const verifyPremiumUser = async () => {
+    try {
+      const response = await axios.get(BASE_URL + "/shop/premium/verify", {
+        withCredentials: true,
+      });
+      if (response.data.isPremium === true) {
+        setPremiumUser(true);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   const paymentTypeHandler = async (membershipType) => {
     try {
       const order = await axios.post(
@@ -14,7 +33,6 @@ const Premium = () => {
 
       console.log(order);
 
-      
       const { orderId, amount, currency, notes, key } = order.data;
 
       const options = {
@@ -33,6 +51,7 @@ const Premium = () => {
         theme: {
           color: "#F37254",
         },
+        handler: verifyPremiumUser,
       };
 
       const rzp = new Razorpay(options);
@@ -45,50 +64,54 @@ const Premium = () => {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="flex w-full flex-col lg:flex-row p-10">
-        <div className="card bg-base-300 rounded-box py-10 grid grow place-items-center">
-          <h1 className="font-bold text-3xl uppercase mb-10">
-            Silver <span className="text-sky-400 underline">Membership</span>
-          </h1>
-          <ol className="list-decimal">
-            <li>50 Backtracking per week</li>
-            <li>6 Months Access</li>
-            <li>Send Direct Message to 20 people per day</li>
-            <li>Share Project Ideas on feed</li>
-            <li>Connect Git Account to share projects</li>
-          </ol>
-          <button
-            onClick={() => {
-              paymentTypeHandler("silver");
-            }}
-            className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
-          >
-            Pay Now
-          </button>
+      {premiumUser ? (
+        "You are already a premium user"
+      ) : (
+        <div className="flex w-full flex-col lg:flex-row p-10">
+          <div className="card bg-base-300 rounded-box py-10 grid grow place-items-center">
+            <h1 className="font-bold text-3xl uppercase mb-10">
+              Silver <span className="text-sky-400 underline">Membership</span>
+            </h1>
+            <ol className="list-decimal">
+              <li>50 Backtracking per week</li>
+              <li>6 Months Access</li>
+              <li>Send Direct Message to 20 people per day</li>
+              <li>Share Project Ideas on feed</li>
+              <li>Connect Git Account to share projects</li>
+            </ol>
+            <button
+              onClick={() => {
+                paymentTypeHandler("silver");
+              }}
+              className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
+            >
+              Pay Now
+            </button>
+          </div>
+          <div className="divider lg:divider-horizontal">OR</div>
+          <div className="card bg-base-300 rounded-box grid py-10 grow place-items-center">
+            <h1 className="font-bold text-3xl mb-10 uppercase">
+              Gold <span className="text-yellow-500 underline">Membership</span>
+            </h1>
+            <ol className="list-decimal">
+              <li>Get a verified Blue Tick</li>
+              <li>Unlimited Backtracking per week</li>
+              <li>12 Months Access</li>
+              <li>Send Unlimited Direct Message</li>
+              <li>Share Project Ideas on feed</li>
+              <li>Connect Git Account to share projects</li>
+            </ol>
+            <button
+              onClick={() => {
+                paymentTypeHandler("gold");
+              }}
+              className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
+            >
+              Pay Now
+            </button>
+          </div>
         </div>
-        <div className="divider lg:divider-horizontal">OR</div>
-        <div className="card bg-base-300 rounded-box grid py-10 grow place-items-center">
-          <h1 className="font-bold text-3xl mb-10 uppercase">
-            Gold <span className="text-yellow-500 underline">Membership</span>
-          </h1>
-          <ol className="list-decimal">
-            <li>Get a verified Blue Tick</li>
-            <li>Unlimited Backtracking per week</li>
-            <li>12 Months Access</li>
-            <li>Send Unlimited Direct Message</li>
-            <li>Share Project Ideas on feed</li>
-            <li>Connect Git Account to share projects</li>
-          </ol>
-          <button
-            onClick={() => {
-              paymentTypeHandler("gold");
-            }}
-            className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
-          >
-            Pay Now
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };

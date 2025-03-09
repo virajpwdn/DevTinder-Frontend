78e078f3844bd0959737f71b00e58c170183a47b
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-09 16:46:05 +0530

    added razor pay popup of payment

--- src/shop/Premium.jsx
+++ src/shop/Premium.jsx
@@ -3,9 +3,49 @@ import React, { useState } from "react";
 import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
+  const [error, setError] = useState("");
+  const paymentTypeHandler = async (membershipType) => {
+    try {
+      const order = await axios.post(
+        BASE_URL + "/shop/payment/create",
+        { membershipType },
+        { withCredentials: true }
+      );
+
+      console.log(order);
+
+      
+      const { orderId, amount, currency, notes, key } = order.data;
+
+      const options = {
+        key: key, // Replace with your Razorpay key_id
+        amount: amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        currency: currency,
+        name: "Dev Tinder",
+        description: "A place where developers can connect",
+        order_id: orderId, // This is the order_id created in the backend
+        // callback_url: "http://localhost:3000/payment-success", // Your success URL
+        prefill: {
+          name: notes.firstName + " " + notes.lastName,
+          email: notes.emailId,
+          contact: "9999999999",
+        },
+        theme: {
+          color: "#F37254",
+        },
+      };
+
+      const rzp = new Razorpay(options);
+      rzp.open();
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div className="h-screen">
-      <div className="flex w-full flex-col lg:flex-row mt-10 p-5">
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex w-full flex-col lg:flex-row p-10">
         <div className="card bg-base-300 rounded-box py-10 grid grow place-items-center">
           <h1 className="font-bold text-3xl uppercase mb-10">
             Silver <span className="text-sky-400 underline">Membership</span>
@@ -17,7 +57,12 @@ const Premium = () => {
             <li>Share Project Ideas on feed</li>
             <li>Connect Git Account to share projects</li>
           </ol>
-          <button className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10">
+          <button
+            onClick={() => {
+              paymentTypeHandler("silver");
+            }}
+            className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
+          >
             Pay Now
           </button>
         </div>
@@ -34,7 +79,12 @@ const Premium = () => {
             <li>Share Project Ideas on feed</li>
             <li>Connect Git Account to share projects</li>
           </ol>
-          <button className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10">
+          <button
+            onClick={() => {
+              paymentTypeHandler("gold");
+            }}
+            className="bg-emerald-500 px-4 py-2 text-black font-semibold rounded-md mt-10"
+          >
             Pay Now
           </button>
         </div>

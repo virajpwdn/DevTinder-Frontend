8647f41148afa55632ace7a606469d2e546710d3
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-10 14:38:12 +0530

    received emits and passed message to room

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { createConnection } from "../utils/socket";
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
   const [messages, setMessages] = useState([{ text: "hello" }]);
-  const check = "you are logged in";
+  const [newMessage, setNewMessage] = useState("");
+
+  const { targetId } = useParams();
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
+  const firstName = userId.firstName;
+
   useEffect(() => {
+    if (!user) return;
     // Whenver page loads mount the socket
-    console.log(createConnection);
     const socket = createConnection();
-    socket.emit("joinChat", { check });
+
+    socket.emit("joinChat", { targetId, userId });
+
+    socket.on("messageReceived", ({ newMessage }) => {
+      console.log(newMessage);
+    });
 
     // After completation umMount socket
     return () => {
@@ -16,10 +29,21 @@ const Chat = () => {
     };
   }, []);
 
+  const setMessageHandler = () => {
+    const socket = createConnection();
+
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      userId: userId,
+      targetId,
+      newMessage,
+    });
+  };
+
   return (
     <div className="p-10 min-h-screen flex items-center justify-start flex-col">
       <h1 className="text-3xl text-left mb-5 font-bold text-zinc-400">Chat</h1>
-      <div className="w-1/3 border border-zinc-300 rounded-md h-[75vh] overflow-scroll relative">
+      <div className="w-3/4 border border-zinc-300 rounded-md h-[75vh] overflow-scroll relative">
         {messages &&
           messages.map((mes, idx) => {
             return (
@@ -61,11 +85,18 @@ const Chat = () => {
           })}
         <div className="send-div flex justify-between p-10 absolute bottom-0 w-full gap-4">
           <input
+            value={newMessage}
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
             type="text"
             className="w-full bg-transparent border border-r-zinc-400 rounded-md p-2 outline-none placeholder:text-zinc-500"
             placeholder="Enter your message"
           />
-          <button className="px-4 py-1.5 bg-emerald-500 text-white rounded-md">
+          <button
+            onClick={setMessageHandler}
+            className="px-4 py-1.5 bg-emerald-500 text-white rounded-md"
+          >
             send
           </button>
         </div>
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,5 @@
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 export const createConnection = () => {
-    // Log it once and check what is comming in createConnection
   return io(BASE_URL);
 };

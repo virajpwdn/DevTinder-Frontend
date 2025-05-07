1fcf5ee0a4c97cd24061e7d194cb5e9f7db6faf3
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-07 14:13:43 +0530

    URL update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
+// export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://devtinder-hx3r.onrender.com/";

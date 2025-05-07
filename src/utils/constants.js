8f98dffa957adfe50cb6223f726a0ebc3d9da34c
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-07 15:56:59 +0530

    new server

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-hl7f.onrender.com";
+
 // export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "https://devtinder-hx3r.onrender.com";
+

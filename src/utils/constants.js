f6b4071f53f3d106a9c166937f4323089e2de109
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-07 18:12:37 +0530

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-hl7f.onrender.com";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-ejvv.onrender.com";
 
 // export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";

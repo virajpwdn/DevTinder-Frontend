f4b8e4389c81bce269e895a74aea358834906061
virajpwdn <virajpatwardhan6@gmail.com>
2025-12-23 20:10:19 +0530

    changed baseUrl for deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-ejvv.onrender.com";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-ejvv.onrender.com";
 
-export const BASE_URL = "http://localhost:80/api";
+// export const BASE_URL = "http://localhost:80/api";
 // export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
 

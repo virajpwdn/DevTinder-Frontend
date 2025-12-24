0fc835553fc075caea6be33a84015832b45b2c58
virajpwdn <virajpatwardhan6@gmail.com>
2025-12-24 11:04:45 +0530

    fix: updated the reverse proxy in nginx config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-ejvv.onrender.com";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder-ejvv.onrender.com";
 
-// export const BASE_URL = "http://localhost:80/api";
+export const BASE_URL = "/api";
 // export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api";
-

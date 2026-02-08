4e09590a5d098937df478721c8b5c896c44cecfa
virajpwdn <virajpatwardhan6@gmail.com>
2026-02-08 13:20:32 +0530

    feat: unit test case

--- src/test/Hello.test.jsx
+++ src/test/Hello.test.jsx
@@ -1,11 +1,19 @@
 import Hello from "../components/Hello";
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import "@testing-library/jest-dom/vitest"
+import "@testing-library/jest-dom/vitest"; //This is the library which gives the following methods like toBeInTheDocument()
 
 describe("hellocomp", () => {
-    it("Renders Hello World by default", () => {
-        render(<Hello />)
-        expect(screen.getByText("Hello World")).toBeInTheDocument();
-    })
-})
+  it("Renders Hello World by default", () => {
+    render(<Hello />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument(); //Assertation
+  });
+  it("Button element check", () => {
+    // render(<Hello />);
+
+    const button = screen.getAllByRole("button");
+    expect(button.length).toBe(2);
+  });
+});
